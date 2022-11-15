9f17eb81111f44f2e021537477196045993f1937
AnkurRajneta <ankur_207003@saitm.org>
2022-11-15 21:20:47 +0530

    first commit

---
+++ starting.swift
@@ -0,0 +1,12 @@
+
+var name="Ted"
+name="Rebecca"
+
+let user="Daphne"
+print(user)
+
+let actor="Tom Cruise 😎"
+let quote="He tapped a sign saying\"Believe\"and walked away."
+print(actor.count)
+print(quote.hasPrefix("He"))
+print(quote.hasSuffix("Away."))
