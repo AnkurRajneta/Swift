fe62e2901be1e0ed6cdd6d140de21149a6f2b7b0
AnkurRajneta <ankur_207003@saitm.org>
2022-11-16 21:03:28 +0530

    Updated swift file

--- starting.swift
+++ starting.swift
@@ -1,3 +1,8 @@
+//for string of multiple line
+let movie="""
+A day in life of an
+Apple engineer
+"""
 let score = 10
 let highscore=score+10
 let finalscore=score/2
@@ -8,4 +13,8 @@ counter += 5
 let number = 120
 print(number.isMultiple(of:3))
 
+print(movie)
+
+//for taking a random number in between 
+let id=Int.random(in: 1...1000)
 
